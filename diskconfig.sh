826b0b50e527a0d6cda29dd075d8fdcb8ac6a3f5
root user <root@localhost.localdomain>
2022-02-28 16:44:58 -0500

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -6,3 +6,6 @@ fi
 
 echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
 mdadm --detail /dev/md0 >>$FILENAME.report
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report

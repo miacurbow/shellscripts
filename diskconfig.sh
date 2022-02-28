c8b1543f99c55d4999e458bfb707b7226891c977
root user <root@localhost.localdomain>
2022-02-28 16:36:17 -0500

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -3,3 +3,6 @@ if [ "$1" = "cool" ] then echo "Cool Beans"
 elif [ "$1" = "neat" ] then echo "Neato Cool"
 else echo "Not Cool Beans"
 fi
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
